7a9d5764e56f095ad1add0d9679c7b593ac6f22f
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-03-02 22:07:13 +0530

    addedCulturesHome
    This is just a home page about the cultures of the Nine Rivers Saga. The details have not been included yet.

---
+++ culturesAndReligions/cultures.js
@@ -0,0 +1,14 @@
+gsap.from (".links", { y: -100, duration: 1, ease: "linear.out"})
+
+gsap.from (".bottomBar", { y: +100, duration: 1,delay: 1, ease: "linear.out"})
+
+gsap.from (".box1", { x: -2000, duration: 1.5,delay: 2, ease: "linear.out"})
+
+gsap.from (".box2", { x: -2000, duration: 2,delay: 2, ease: "linear.out"})
+
+gsap.from (".box3", { x: -2000, duration: 2.5,delay: 2, ease: "linear.out"})
+
+gsap.from (".box4", { x: -2000, duration: 3,delay: 2, ease: "linear.out"})
+
+
+
