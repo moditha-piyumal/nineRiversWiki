3407f237ec2c0e79d620cc1f581ab2e6415d9477
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-04-12 08:52:20 +0530

    Edited Busarish2

---
+++ culturesAndReligions/busarish.js
@@ -0,0 +1,5 @@
+gsap.from ("#location", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+
+gsap.from ("#busarish", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+
+gsap.from ("#tribes", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
