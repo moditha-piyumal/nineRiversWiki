6b574f7925cc5172191b354a7d2d853baeea18c1
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-07-10 22:24:31 +0530

    smallEditBusarish

--- culturesAndReligions/busarish.js
+++ culturesAndReligions/busarish.js
@@ -8,3 +8,11 @@ function locationFun() {
    gsap.from ("#location", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
 
 }
+
+function busarishFun() {
+   gsap.from ("#busarish", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+}
+
+function tribesFun() {
+   gsap.from ("#tribes", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+}
