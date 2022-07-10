1b3d866a7804ec425bd614a305c9f92f68e24121
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-07-11 03:26:41 +0530

    testingGSAPWithWalladia

--- culturesAndReligions/walladia.js
+++ culturesAndReligions/walladia.js
@@ -8,10 +8,46 @@ var tween = gsap.to(".red", {
    paused: true
  });
 
+
+var tween2 = gsap.to(".green", {
+   duration: 4, 
+   x: 750, 
+   rotation: 360, 
+   ease: "none", 
+   paused: true
+ });
+
+
+var tween3 = gsap.to(".blue", {
+   duration: 4, 
+   x: 750, 
+   rotation: 360, 
+   ease: "none", 
+   paused: true
+ });
+
+
+var tween4 = gsap.to(".purple", {
+   duration: 4, 
+   x: 750, 
+   rotation: 360, 
+   ease: "none", 
+   paused: true
+ });
+
 //  gsap.to(".red", {rotation: 27, x: 100, duration: 1});
 
 //  document.querySelector("#joonist").onclick = () => tween.play();
  document.querySelector(".joonist").onclick = () => tween.restart();
+
+
+ document.querySelector(".ukirin").onclick = () => tween2.restart();
+
+
+ document.querySelector(".lost").onclick = () => tween3.restart();
+
+
+ document.querySelector(".reservoir").onclick = () => tween4.restart();
 
 
 //  function funcJoonist() {
