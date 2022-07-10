865747d28c632f6acfdc7f9aff10c61ad9f000f8
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-07-10 22:14:11 +0530

    busarishAnimationRestart

--- culturesAndReligions/busarish.js
+++ culturesAndReligions/busarish.js
@@ -1,5 +1,10 @@
-gsap.from ("#location", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
 
-gsap.from ("#busarish", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+// gsap.from ("#busarish", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
 
-gsap.from ("#tribes", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+// gsap.from ("#tribes", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+
+function locationFun() {
+
+   gsap.from ("#location", { x: +2000, duration: 1.5,delay: 0, ease: "linear.out"})
+
+}
