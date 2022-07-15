a7d35dceb42fca6cb9bbc443142672428efb7a79
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-07-15 21:59:35 +0530

    updateJS

--- summaryOfTheStory/summary.js
+++ summaryOfTheStory/summary.js
@@ -11,25 +11,25 @@ gsap.from("#h", {rotation: 1800, x: -2000, duration: 8, ease: "bounce"});
 gsap.from("#i", {rotation: 3600, x: -2000, duration: 7, ease: "bounce"});
 gsap.from("#j", {rotation: 1800, x: -2200, duration: 6, ease: "bounce"});
 
-gsap.from("#k", {x: 1000, duration: 6});
-gsap.to("#k", {x: -2000, duration: 6, delay: 6});
+gsap.from("#k", {x: 1000, duration: 10});
+gsap.to("#k", {x: -2000, duration: 6, delay: 10});
 
 gsap.from("#l", {x: 1000, duration: 5});
-gsap.to("#l", {x: -2000, duration: 4, delay: 5});
+gsap.to("#l", {x: -2000, duration: 8, delay: 5});
 
-gsap.from("#m", {x: 1000, duration: 4});
-gsap.to("#m", {x: -2000, duration: 3, delay: 4});
-
-
-gsap.from("#n", {x: -1000, duration: 2});
-gsap.to("#n", {x: 2000, duration: 6, delay: 2});
-
-gsap.from("#o", {x: -1000, duration: 3});
-gsap.to("#o", {x: 2000, duration: 4, delay: 3});
+gsap.from("#m", {x: 1000, duration: 9});
+gsap.to("#m", {x: -2000, duration: 3, delay: 9});
 
 
-gsap.from("#p", {x: -1000, duration: 2}),
-gsap.to("#p", {x: 2000, duration: 3, delay: 2});
+gsap.from("#n", {x: -1000, duration: 7});
+gsap.to("#n", {x: 2000, duration: 6, delay: 7});
+
+gsap.from("#o", {x: -1000, duration: 8});
+gsap.to("#o", {x: 2000, duration: 4, delay: 8});
+
+
+gsap.from("#p", {x: -1000, duration: 9}),
+gsap.to("#p", {x: 2000, duration: 3, delay: 9});
 
 
 
