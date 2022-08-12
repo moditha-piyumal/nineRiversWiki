5c8cd8e33120f155ef712decc80b9ba705e0c821
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-08-12 21:47:27 +0530

    ukirinReligionPageNew

---
+++ culturesAndReligions/ukirinRel.js
@@ -0,0 +1,25 @@
+var box = document.querySelector("#box");
+var box1 = document.querySelector('#box1');
+
+
+const fulltime = 6000;
+const halftime = 3000;
+
+anim()
+
+function anim() {
+
+   box.className = 'grow'
+
+   box1.className = 'grow'
+
+   setTimeout(() => {
+      box.className = 'shrink'
+   }, halftime)
+
+   setTimeout(() => {
+      box1.className = 'shrink'
+   }, halftime)
+}
+
+setInterval(anim, fulltime);
