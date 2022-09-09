9f1b9919e1c1ae77cf0bf775c161027b095f52f1
Moditha Piyumal Wijeratne <100229199+moditha-piyumal@users.noreply.github.com>
2022-09-09 11:58:08 +0530

    updateBisonHearders

--- culturesAndReligions/joonistBisonHearders/joonistBison.js
+++ culturesAndReligions/joonistBisonHearders/joonistBison.js
@@ -1,4 +1,12 @@
 function ballBig() {
    var ball = document.getElementById("x");
+   var btn = document.querySelector(".btn");
+
    ball.classList.toggle("animX");
+
+   if (btn.innerHTML === "Animate") {
+      btn.innerHTML = "Unanimate";
+   } else {
+      btn.innerHTML = "Animate";
+   }
 };
